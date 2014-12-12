7689c522e0921ba3496b1b41960473281d6c09da
Bea <bea_riego93@hotmail.com>
2014-12-12 12:10:25 +0100

    commit 1: creamos un metodo muestraDatos para que muestre la informacion

---
+++ Alimento.java
@@ -0,0 +1,39 @@
+
+/**
+ * Write a description of class Alimento here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Alimento
+{
+    // instance variables - replace the example below with your own
+    private String nombreAlimento;
+    private float gramosProteinas;
+    private float gramosCarbohidratos;
+    private float gramosGrasas;
+    private float gramosCalorias;
+    private String compMayor;
+
+    /**
+     * Constructor for objects of class Alimento
+     */
+    public Alimento(String nombreAlimento, String compMayor, float proteinas, float carbohidratos, float grasas, float calorias)
+    {
+        // initialise instance variables
+        this.nombreAlimento = nombreAlimento;
+        proteinas = 0;
+        carbohidratos = 0;
+        grasas = 0;
+        calorias = 0;
+        this.compMayor = compMayor;
+    }
+    
+         
+    public void muestraDatos()
+    {
+        System.out.println("Nombre: " + nombreAlimento + "\n" + "Proteinas: " + gramosProteinas
+        + "\n" + "Carbohidratos: " + gramosCarbohidratos + "\n" + "Grasas: " + gramosGrasas
+        + "\n" + "Calorias: " + gramosCalorias + "\n" + "Componente mayor: " + compMayor);
+    }
+}
